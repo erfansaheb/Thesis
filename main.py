85c4d46c829bb33763ead7cf5c24c4379fafa23e
erfan.saheb <erfan@asistobe.no>
2023-01-22 14:19:56 +0100

    first working version for main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from Utils import (
     load_problem,
-    # load_solution,
-    # feasibility_check,
     cost_function,
 )
 from time import time
@@ -10,12 +8,9 @@ from Operators import one_week_swap, multi_week_swap, one_game_flip, multi_game_
 import numpy as np
 import pandas as pd
 
-
-# sol = np.ones((problem["n_teams"], problem["n_teams"]), dtype=int) * (-1)
-# solution, objective_value = load_solution('..//Appendix_Files//Final_Solutions//Early_instances//E2.xml', sol)
 if __name__ == "__main__":
     problem = load_problem("Instances//EarlyInstances_V3//ITC2021_Early_2.xml")
-    initial_sol = pd.read_csv("sol.csv", header=None).to_numpy()
+    initial_sol = pd.read_csv("Erfan_init_sol.csv", header=None).to_numpy()
     init_cost = cost_function(initial_sol, problem)
     operators = [one_week_swap, multi_week_swap, one_game_flip, multi_game_flip]
     operators_len = len(operators)
@@ -39,7 +34,7 @@ if __name__ == "__main__":
                 last_improvement[i],
                 weights[i],
                 feas_sols[i],
-            ) = ALNS(initial_sol, init_cost, prb, operators, problem, rng)
+            ) = ALNS(initial_sol, init_cost, prb, operators, [0, 1, 2, 3], problem, rng)
             running_time = (time() - start) / repeat
             minidx = np.argmin(best_cost)
             print(
