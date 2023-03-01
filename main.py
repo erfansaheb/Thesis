140c76de9be5537aa75301a27063441acec40cb5
erfan.saheb <erfan@asistobe.no>
2023-03-01 14:27:23 +0100

    Update to be compatible with Solution class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from app.utils import (
     cost_function,
+    random_init_sol,
 )
 from app.load import load_problem
 from time import time
@@ -8,10 +9,18 @@ from app.Operators import one_week_swap, multi_week_swap, one_game_flip, multi_g
 import numpy as np
 import pandas as pd
 
+from app.named_tuples import Solution
+
 if __name__ == "__main__":
-    problem = load_problem("Instances//EarlyInstances_V3//ITC2021_Early_2.xml")
-    initial_sol = pd.read_csv("Erfan_init_sol.csv", header=None).to_numpy()
-    init_cost = cost_function(initial_sol, problem)
+    problem = load_problem("Instances//ITC2021_Early_2.xml")
+    solution = Solution(
+        problem=problem,
+        representative=random_init_sol(
+            problem, rng=np.random.default_rng(3)
+        ),  # pd.read_csv("Erfan_init_sol.csv", header=None).to_numpy(),
+    )
+    # initial_sol = pd.read_csv("Erfan_init_sol.csv", header=None).to_numpy()
+    init_cost = solution.total_cost
     operators = [one_week_swap, multi_week_swap, one_game_flip, multi_game_flip]
     operators_len = len(operators)
     probabilities = [[1 / operators_len for _ in operators]]
@@ -19,33 +28,34 @@ if __name__ == "__main__":
     repeat_range = range(repeat)
     for prb in probabilities:
         start = time()
-        best_sol, best_cost, last_improvement, weights, feas_sols = (
-            [[] for _ in repeat_range],
-            [0 for _ in repeat_range],
+        best_sol, last_improvement, weights, feas_sols = (
+            [solution.copy() for _ in repeat_range],
             [0 for _ in repeat_range],
             [[] for _ in repeat_range],
             [[] for _ in repeat_range],
         )
         for i in repeat_range:
             rng = np.random.default_rng(31 + i)
-            (
-                best_sol[i],
-                best_cost[i],
-                last_improvement[i],
-                weights[i],
-                feas_sols[i],
-            ) = ALNS(initial_sol, init_cost, prb, operators, [0, 1, 2, 3], problem, rng)
-            running_time = (time() - start) / repeat
-            minidx = np.argmin(best_cost)
-            print(
+            (best_sol[i], last_improvement[i], weights[i], feas_sols[i],) = ALNS(
+                solution,
                 prb,
-                "\t",
-                np.mean(best_cost),
-                "\t",
-                best_cost[minidx],
-                "\t",
-                100 * ((init_cost - best_cost[minidx]) / init_cost),
-                "\t",
-                running_time,
+                operators,
+                [0, 1, 2, 3],
+                problem,
+                rng,
             )
-            print("Solution: ", best_sol[minidx])
+        running_time = (time() - start) / repeat
+        best_cost = [sol.total_cost for sol in best_sol]
+        minidx = np.argmin(best_cost)
+        print(
+            prb,
+            "\t",
+            np.mean(best_cost),
+            "\t",
+            best_cost[minidx],
+            "\t",
+            100 * ((init_cost - best_cost[minidx]) / init_cost),
+            "\t",
+            running_time,
+        )
+        print("Solution: ", best_sol[minidx])
