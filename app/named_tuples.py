ff1562a54b9813c19f8a34691b0dd065ef4c03bb
erfan.saheb <erfan@asistobe.no>
2023-03-01 14:26:04 +0100

    Update Solution attributes add Constraint class

--- app/named_tuples.py
+++ app/named_tuples.py
@@ -1,6 +1,9 @@
 # from collections import namedtuple
 
+from itertools import product
 import numpy as np
+from copy import deepcopy
+from app.utils import cost_function
 
 
 class Solution:
@@ -9,13 +12,67 @@ class Solution:
     teams_cost: np.array
     games_cost: np.array
     slots_cost: np.array
+    hard_cost: int = 0
+    soft_cost: int = 0
+    obj_fun: int = 0
+    problem: dict
 
-    def __init__(self, representative, total_cost, teams_cost, games_cost, slots_cost):
+    def __init__(
+        self,
+        problem,
+        representative=None,
+        total_cost=0,
+        hard_cost=0,
+        soft_cost=0,
+        obj_fun=None,
+    ):
+        self.problem = problem
         self.representative = representative
         self.total_cost = total_cost
-        self.teams_cost = teams_cost
-        self.games_cost = games_cost
-        self.slots_cost = slots_cost
+        self.teams_cost = np.zeros(problem["n_teams"])
+        self.games_cost = np.zeros((problem["n_teams"], problem["n_teams"]))
+        self.slots_cost = np.zeros(problem["n_slots"])
+        self.hard_cost = hard_cost
+        self.soft_cost = soft_cost
+        self.obj_fun = obj_fun or cost_function(self, problem)
+
+    def copy(self):
+        return deepcopy(self)
+
+
+class Constraint:
+    all: list
+    teams: dict[int, list]
+    slots: dict[int, list]
+    games: dict[tuple[int, int], list]
+    types: dict = {"ca": 4, "ba": 2, "ga": 1, "fa": 1, "sa": 1}
+
+    def __init__(self, n_teams, n_slots):
+        self.all = {
+            type: [[] for _ in range(n_types)] for type, n_types in self.types.items()
+        }
+        self.teams = {
+            i: {
+                type: [[] for _ in range(n_types)]
+                for type, n_types in self.types.items()
+            }
+            for i in range(n_teams)
+        }
+        self.slots = {
+            i: {
+                type: [[] for _ in range(n_types)]
+                for type, n_types in self.types.items()
+            }
+            for i in range(n_slots)
+        }
+        self.games = {
+            (i, j): {
+                type: [[] for _ in range(n_types)]
+                for type, n_types in self.types.items()
+            }
+            for i, j in product(range(n_teams), range(n_teams))
+            if i != j
+        }
 
 
 # Solution = namedtuple(
