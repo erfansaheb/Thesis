a44cd5535715f8631fe79b7aa63207227e0f6a14
erfan.saheb <erfan@asistobe.no>
2023-09-05 23:30:26 +0200

    modulize circle method

--- app/circle_method.py
+++ app/circle_method.py
@@ -7,24 +7,34 @@ Created on Fri Jul 29 17:14:45 2022
 
 import numpy as np
 
-num_teams = 20  # input('\nEnter number of teams: - ')
-# teams_list = []
-teams_list = list(np.random.permutation(np.arange(1, num_teams + 1)))
-# for i in range(int(num_teams)):
-#     teams_list.append(i+1)
-if (len(teams_list) % 2) != 0:
-    teams_list.append(0)
-    # team_list = np.append(teams_list,0)
-x = teams_list[: len(teams_list) // 2]
-y = teams_list[len(teams_list) // 2 :]
-matches = []
-for i in range(len(teams_list) - 1):
-    rounds = {}
-    if i != 0:
-        x.insert(1, y.pop(0))
-        y.append(x.pop())
-    matches.append(rounds)
-    for j in range(len(x)):
-        rounds[x[j]] = y[j]
-for i in range(len(matches)):
-    print("\n Day", i + 1, " : ", matches[i])
+
+def circle_method(num_teams, rng):
+    # num_teams = 20  # input('\nEnter number of teams: - ')
+    # teams_list = []
+    teams_list = list(rng.permutation(np.arange(0, num_teams)))
+    # for i in range(int(num_teams)):
+    #     teams_list.append(i+1)
+    if (len(teams_list) % 2) != 0:
+        teams_list.append(0)
+        # team_list = np.append(teams_list,0)
+    x = teams_list[: len(teams_list) // 2]
+    y = teams_list[len(teams_list) // 2 :]
+    Vars = []
+    for i in range(len(teams_list) - 1):
+        # rounds = {}
+        if i != 0:
+            x.insert(1, y.pop(0))
+            y.append(x.pop())
+        # matches.append(rounds)
+        Vars.extend({"VarName": f"x[{x[j]},{y[j]},{i}]", "X": 1} for j in range(len(x)))
+        Vars.extend(
+            {"VarName": f"x[{y[j]},{x[j]},{i+len(teams_list) - 1}]", "X": 1}
+            for j in range(len(x))
+        )
+        # rounds[x[j]] = y[j]
+    # for i in range(len(matches)):
+    #     print("\n Day", i + 1, " : ", matches[i])
+    return Vars
+
+
+# print(circle_method(5))
